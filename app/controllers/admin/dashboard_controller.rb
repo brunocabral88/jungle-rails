13e0dbe0acbb1447066b66290ca0666e1acd321e
Bruno Cabral <bruno.cabral88@hotmail.com>
2017-04-13 19:22:26 +0000

    Implement basic authentication to admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_PAGES_USERNAME'], password: ENV['ADMIN_PAGES_PASSWORD']
+  
   def show
   end
 end
